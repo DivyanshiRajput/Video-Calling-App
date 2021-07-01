a1bb618108b653b770b6fe69fbca56a5a933116e
DivyanshiRajput <divyanshi.rajput@iiitb.ac.in>
2021-07-01 14:18:45 +0530

    added feedback function

--- public/leave.js
+++ public/leave.js
@@ -2,16 +2,8 @@ function rejoin(){
   window.history.back();
 };
 
-var firebaseConfig = {
-    apiKey: "AIzaSyAF1i5MloeErcq7ErFcnEEsEg4CGM5Enps",
-    authDomain: "teams-clone-a921c.firebaseapp.com",
-    databaseURL: "https://teams-clone-a921c-default-rtdb.firebaseio.com",
-    projectId: "teams-clone-a921c",
-    storageBucket: "teams-clone-a921c.appspot.com",
-    messagingSenderId: "24512433115",
-    appId: "1:24512433115:web:85d0bd97e77414338e75fe",
-    measurementId: "G-R1EGVPHZ3R"
-  };
+var firebaseConfig = FIREBASE_CONFIG;
+
 firebase.initializeApp(firebaseConfig);
 const db = firebase.database();
 
