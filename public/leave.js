80c9f8a53f4f16c1e3a8e1ee73665d485505676d
DivyanshiRajput <divyanshi.rajput@iiitb.ac.in>
2021-07-01 14:07:15 +0530

    added feedback on leave page

--- public/leave.js
+++ public/leave.js
@@ -1,7 +1,33 @@
 function rejoin(){
   window.history.back();
-}
+};
 
-// $("form").serializeArray(() => {
-//   console.log("taken input");
-// });
+var firebaseConfig = {
+    apiKey: "AIzaSyAF1i5MloeErcq7ErFcnEEsEg4CGM5Enps",
+    authDomain: "teams-clone-a921c.firebaseapp.com",
+    databaseURL: "https://teams-clone-a921c-default-rtdb.firebaseio.com",
+    projectId: "teams-clone-a921c",
+    storageBucket: "teams-clone-a921c.appspot.com",
+    messagingSenderId: "24512433115",
+    appId: "1:24512433115:web:85d0bd97e77414338e75fe",
+    measurementId: "G-R1EGVPHZ3R"
+  };
+firebase.initializeApp(firebaseConfig);
+const db = firebase.database();
+
+function addFeedback(){
+  const ref = db.ref('Ratings/' + Date.now());
+  if($("input[type='radio'][name='rating']:checked").val() > 0){
+    var temp = $("input[type='radio'][name='rating']:checked").val();
+    temp = Number(temp);
+  }
+  else{
+    var temp = 0;
+  }
+  var newRating = {
+    rating: temp,
+  };
+
+  ref.set(newRating);
+  alert("Your feedback has been submitted.")
+};
