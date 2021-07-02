ff1353be578ef343f61f5c3d00935417c7bc89bf
DivyanshiRajput <divyanshi.rajput@iiitb.ac.in>
2021-07-02 13:55:16 +0530

    added route to home page

--- server.js
+++ server.js
@@ -14,9 +14,13 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use('/peerjs', peerServer);
 
-app.get('/', function(req, res){
+app.get('/create', function(req, res){
     res.redirect(`/${uuidv4()}`);
 });
+
+app.get('/', function(req, res){
+    res.render('index');
+})
 
 app.get('/:room', function(req, res){
     res.render('room', {roomId: req.params.room});
@@ -27,14 +31,19 @@ app.get('/:room/leave', function(req, res){
 })
 
 socketIo.on('connection', socket => {
-    socket.on('join-room', (roomId, userId, userName) => {
+
+        socket.on('join-room', (roomId, userId, userName) => {
         socket.join(roomId);
-        socket.to(roomId).emit('user-connected', userId);
+        socket.to(roomId).emit('user-connected', userId, userName);
         console.log("joined room");
 
         socket.on('message', function(message){
             socketIo.to(roomId).emit('createMessage', message, userName);
         });
+
+        // socket.on('disconnect', (reason) => {
+        // socket.broadcast.emit('client-disconnected', { 'client-id': socket.id });
+        // });
     });
 });
 
