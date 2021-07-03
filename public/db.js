a73bc952e7fe06cf23ec0136094b67061d298890
DivyanshiRajput <divyanshi.rajput@iiitb.ac.in>
2021-07-03 21:59:39 +0530

    added config file

---
+++ public/db.js
@@ -0,0 +1,10 @@
+var FIREBASE_CONFIG = {
+     apiKey: "AIzaSyAF1i5MloeErcq7ErFcnEEsEg4CGM5Enps",
+     authDomain: "teams-clone-a921c.firebaseapp.com",
+     databaseURL: "https://teams-clone-a921c-default-rtdb.firebaseio.com",
+     projectId: "teams-clone-a921c",
+     storageBucket: "teams-clone-a921c.appspot.com",
+     messagingSenderId: "24512433115",
+     appId: "1:24512433115:web:85d0bd97e77414338e75fe",
+     measurementId: "G-R1EGVPHZ3R"
+   };
