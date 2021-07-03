8b5c9ea9eaaf4636735d9459a2243aad7b296da5
DivyanshiRajput <divyanshi.rajput@iiitb.ac.in>
2021-07-03 23:57:20 +0530

    added cumulative rating function

--- public/leave.js
+++ public/leave.js
@@ -12,7 +12,7 @@ firebase.initializeApp(firebaseConfig);
 const db = firebase.database();
 
 function addFeedback(){
-  
+
     const ref = db.ref('Ratings/' + Date.now());
     if($("input[type='radio'][name='rating']:checked").val() > 0){
         var temp = $("input[type='radio'][name='rating']:checked").val();
@@ -26,5 +26,23 @@ function addFeedback(){
     };
 
     ref.set(newRating);
-    alert("Your feedback has been submitted.")
+
+    // old_avg = Number();
+    // count = Number();
+    // // update average
+    // const avg_ref = db.ref('Average/avg');
+    // avg_ref.once('value',(snap)=>{
+    //   old_avg = snap.val()});
+    //
+    // const count_ref = db.ref('Average/count');
+    // count_ref.once('value',(snap)=>{
+    //   count = snap.val()});
+    //
+    // new_avg = ((old_avg * count) + temp )/(count + 1);
+    // avg_ref.set(new_avg);
+    //
+    // count += 1;
+    // count_ref.set(count);
+
+    alert("Your feedback has been submitted.");
 };
