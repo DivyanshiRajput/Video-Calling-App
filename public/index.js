5b36a91c95af200d715bccb4e7b04ca309c1f6cc
DivyanshiRajput <divyanshi.rajput@iiitb.ac.in>
2021-07-04 18:10:52 +0530

    added validation function for join room link

--- public/index.js
+++ public/index.js
@@ -13,7 +13,8 @@ function redirectToCreateRoom(){
 }
 
 function joinRoom(){
-  if (document.getElementById("room-link").value == ""){
+  var url = document.getElementById("room-link").value;
+  if ( !validURL(url)){
     alert("Please enter a valid link.");
   }
 
@@ -50,4 +51,14 @@ function showSlides(n) {
   }
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
+}
+
+const validURL = (str) => {
+  var pattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
+    '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // domain name
+    '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
+    '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
+    '(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
+    '(\\#[-a-z\\d_]*)?$','i'); // fragment locator
+  return !!pattern.test(str);
 }
