314976d8efdc1d63edb7b7080534d118387facf9
DivyanshiRajput <divyanshi.rajput@iiitb.ac.in>
2021-07-08 14:22:25 +0530

    handle chatroom requests

--- server.js
+++ server.js
@@ -50,7 +50,7 @@ app.get('/:room', function(req, res){
 });
 
 app.get('/:room/leave', function(req, res){
-    res.render('leave');
+    res.render('leave', {roomId: req.params.room});
 })
 
 app.get('/:room/chatroom', function(req, res){
